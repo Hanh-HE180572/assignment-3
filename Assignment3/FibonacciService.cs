a150e83309586c5ff16139d7cad1289422108945
hoangson-1234 <danghoangson2752k4@gmail.com>
2026-02-01 10:45:44 +0700

    Add Fibonacci recursive implementation (impl1)

---
+++ Assignment3/FibonacciService.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment3.danghoangson.module1
+{
+    public class FibonacciService : IFibonacci
+    {
+        public int Fibonacci(int n, bool flag)
+        {
+            if (!flag)
+            {
+                return -1;
+            }
+
+            if (n <= 1)
+            {
+                return n;
+            }
+            return Fibonacci(n - 1, true) + Fibonacci(n - 2, true);
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Assignment3.danghoangson.module1;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFibonacci fib = new FibonacciService();
+
+            int n = 6;
+            bool flag = true;
+
+            int result = fib.Fibonacci(n, flag);
+
+            Console.WriteLine($"Fibonacci({n}) = {result}");
+        }
+    }
+}
