f65ba55321d7a77f195f26e9c8c364850a10b1c9
ducddhe180839 <dangduc141119@gmail.com>
2026-02-01 10:27:05 +0700

    Tính Fibonacci bằng vòng lặp for khi flag = false

--- FibonacciModule.cs
+++ FibonacciModule.cs
@@ -4,6 +4,18 @@ public class FibonacciModule
 {
     public long Fibonacci(int n, bool flag)
     {
+        if (flag == false) 
+        {
+            if (n <= 1) return n;
+            long a = 0, b = 1, temp;
+            for (int i = 2; i <= n; i++)
+            {
+                temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
         return 0;
     }
 }
